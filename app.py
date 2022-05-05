b07f91b1c494b327c3f75f22c025d7affbf1cfa5
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2022-05-05 12:11:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+#def hello_world():
     #return render_template("index.html")
-    a = 2
+  #  a = 2
     print 'Hello There, Welcome To Your App!'
-    exit()
+exit()
+    
+
