2957948400dfce7fc189e4b13e0ddb3f2a078d73
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2022-06-12 12:20:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import requests
 from flask import Flask
 from flask import render_template
 
@@ -8,8 +7,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     #return render_template("index.html")
-    response = requests.post('https://curlpattern22-hjfqtyb23a-uc.a.run.app/v1/models/default:predict', data={'url':'https://adalo-uploads.imgix.net/ee6ba0ead111d5deb1520f0ae2c4d9e2fd9831db5b8c1a544a7552ff9b186f5c.jpeg?orient'})
-    output = response.content
     return "Hello There, Welcome To Your App!"
 
   
