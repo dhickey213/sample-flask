10bfb740d9ade7287d974fe000fe4d5d3e3b619a
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2022-06-13 10:36:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask import render_template
-
+import sys
+import pprint
 
 app = Flask(__name__)
 
+pprint.pprint(sys.path)
 
-@app.route("/")
-def hello_world():
+#@app.route("/")
+#def hello_world():
     #return render_template("index.html")
-    return "Hello There, Welcome To Your App!"
+ #   return "Hello There, Welcome To Your App!"
 
   
    
