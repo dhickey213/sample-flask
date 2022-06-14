d2884fe15f4c8659bb722d88a3e8afed61d15386
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2022-06-14 11:45:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,21 @@ import urllib.request
 import requests
 
 app = Flask(__name__)
-imgurls = {'url':'text'}
+
+def get_prediction(url):
+    with urllib.request.urlopen(url) as URL:
+        my_string=base64.b64encode(URL.read()).decode('utf-8')
+    payload = {"instances":[{"image_bytes":{"b64":my_string},"key":"mykey"}]}
+    jsonstring = json.dumps(payload)
+    response = requests.post('https://curlpattern22-hjfqtyb23a-uc.a.run.app/v1/models/default:predict', data=jsonstring)
+    output = response.content
+    return output
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
     data = json.loads(request.data)
-    output = data['url']
+    newurl = data['url']
+    output = get_prediction(newurl)
     return output
  
 #    headers = request.headers
