72ae05a542ff9edb830f0a3cc1204f94f8bec650
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2022-06-15 16:08:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,10 +21,11 @@ def hello_world():
     data = json.loads(request.data)
     newurl = data['url']
     output = get_prediction(newurl)
-    labeldict = json.load(output)
-    max_value= max(labeldict['predictions'][0]['scores'])
-    max_index= labeldict['predictions'][0]['scores'].index(max_value)
-    label_output= labeldict['predictions'][0]['labels'][max_index]
+    #labeldict = json.load(output)
+   # max_value= max(labeldict['predictions'][0]['scores'])
+   # max_index= labeldict['predictions'][0]['scores'].index(max_value)
+   # label_output= labeldict['predictions'][0]['labels'][max_index]
+    label_output = (type(output))
     return label_output
  
 #    headers = request.headers
