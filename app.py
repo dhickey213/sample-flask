453ba7ae516cb7f9a0259051299819c323d7cd4b
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2023-11-20 15:31:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -54,10 +54,9 @@ def hello_world():
         payload = {"Start Appointment": key, "End Appointment":value, "Available": "true"}
         payloadjson = json.dumps(payload)
         time.sleep(.4)
-        response = requests.post(url, params=params, data = payloadjson, headers=headers)
+    #    response = requests.post(url, params=params, data = payloadjson, headers=headers)
         output_me = json.loads(response.content)
-        print("...working...")
-        return output_me
+        print(output_me)
 
 
     
