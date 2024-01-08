71721f06c898090fdee03173ac9682247484e145
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2024-01-08 17:30:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -147,15 +147,12 @@ def create_appts(data):
         endweeklylist = []
         weeklyloop = 0
     
-        while startweekly <= endrepeat and weeklyloop<15:
+        while startweekly < endrepeat and weeklyloop <9:
             weeklyloop += 1
-            time.sleep(.05)
             for i in range(len(starttimelist)):
-                #startweekly = starttimelist[i] + datetime.timedelta(weeks=1)
-                #endweekly = endtimelist[i] + datetime.timedelta(weeks=1)
                 startweeklylist.append(starttimelist[i] + datetime.timedelta(weeks=1))
                 endweeklylist.append(starttimelist[i] + datetime.timedelta(weeks=1))
-                startweekly = startweekly + datetime.timedelta(weeks=1)            
+            startweekly = startweekly + datetime.timedelta(weeks=1)            
 
         starttimelist.extend(startweeklylist)
         endtimelist.extend(endweeklylist)
