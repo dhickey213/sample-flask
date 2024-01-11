e866dc91fd03ee0d8116aafee8cad6410856c9f2
cuebeyco <72028539+dhickey213@users.noreply.github.com>
2024-01-11 13:30:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,12 +50,12 @@ def single_appt_repeat(data):
   #  starttime = data['starttime'].replace("T"," ")
   #  starttime = starttime.replace("Z","")
   #  starttime = datetime.datetime.strptime(starttime, '%Y-%m-%d %H:%M:%S')
-    starttime = datetime.datetime.fromtimestamp(int(data['starttime']))
-    endtime = datetime.datetime.fromtimestamp(int(data['endtime']))
+    starttime = datetime.datetime.fromtimestamp((int(data['starttime'])*86400))
+    endtime = datetime.datetime.fromtimestamp((int(data['endtime'])*86400))
   #  endtime = data['endtime'].replace("T"," ")
   #  endtime = endtime.replace("Z","")
   #  endtime = datetime.datetime.strptime(endtime, '%Y-%d-%m %H:%M:%S')
-    endrepeat = datetime.datetime.fromtimestamp((int(data['endrepeat']))-604800)
+    endrepeat = datetime.datetime.fromtimestamp(((int(data['endrepeat']))*86400)-604800)
     
     dayiterations = 0
     newstartlist = []
